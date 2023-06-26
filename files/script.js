4cb4b762da3c96474665b96fd2bf8f4648a31bed
Kamel Amen <kamelamen38@gmail.com>
2023-06-26 10:28:19 +0300

    Adding loader

--- files/script.js
+++ files/script.js
@@ -1,1 +1,8 @@
-AOS.init();
+window.addEventListener('load', () => {
+  AOS.init();
+  const loader = document.querySelector('.loader');
+  loader.classList.add('loader-hidden');
+  loader.addEventListener('transitioned', () => {
+    document.body.removeChild('loader');
+  });
+});
